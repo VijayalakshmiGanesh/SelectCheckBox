fd78e13848bb59c4eb2784a87ba912605513fcb8
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-01-04 13:40:44 +0530

    Added logic to display selected values

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 let checkboxes = document.querySelectorAll("input[type='checkbox']")
 let lastChecked = null;
+let result = document.querySelector(".result")
 
 
+function displayResult(){
+    let resultValuesSet = new Set();
+    checkboxes.forEach(checkbox =>{
+        if(checkbox.checked)
+            resultValuesSet.add(checkbox.value)
+    })
+    result.innerHTML="The Selected values are: <br/><ul>"
+    resultValuesSet.forEach( value => result.innerHTML+=`<li>${value}</li>`)
+    result.innerHTML+=`</ul>`
+}
 function clickHandler(e) {
     let inbetween = false;
 
@@ -16,8 +27,12 @@ function clickHandler(e) {
         })
     }
     lastChecked = this;
+    displayResult()
 }
-checkboxes.forEach(checkbox => {
+
+checkboxes.forEach(checkbox => { 
     checkbox.addEventListener("click", clickHandler)
 })
+
+
 
